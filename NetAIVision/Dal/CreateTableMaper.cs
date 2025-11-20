46981d19e8314b4f498e6f695847e4fd2f0b4a81
Shengbi <39143915+Countonme@users.noreply.github.com>
2025-11-20 11:44:01 +0800

    Add sqlite support to project  record test data

---
+++ NetAIVision/Dal/CreateTableMaper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetAIVision.Dal
+{
+    public class CreateTableMaper
+    {
+        private SqlLiteHelper _db = null;
+
+        public CreateTableMaper(SqlLiteHelper db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// 创建测试记录表
+        /// </summary>
+        public void CreateTest_RecordTable()
+        {
+            // 创建一个名为 "test_record" 的表格，仅在不存在时创建
+            string createTableQuery = $"CREATE TABLE IF NOT EXISTS main.test_record (" +
+
+                $" \"serial_number\" text(60)," +
+                $" \"test_result\" text(60)," +
+                $" \"version\" text(60)," +
+                $" \"test_datetime\" text(60)" +
+
+                $" );";
+
+            // 设置外键约束
+            // string enableForeignKeyQuery = "PRAGMA foreign_keys = true;";
+
+            // 执行查询
+            // _db.ExecuteNonQuery(enableForeignKeyQuery);
+            _db.ExecuteNonQuery(createTableQuery);
+        }
+    }
+}
---
+++ NetAIVision/Dal/sqlLiteHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetAIVision.Dal
+{
+    public class SqlLiteHelper
+    {
+        private string connectionString;
+
+        public SqlLiteHelper(string dbPath)
+        {
+            connectionString = $"Data Source={dbPath};Version=3;";
+            AutoCeratedb();
+        }
+
+        /// <summary>
+        /// 如果没有db 就自动建立
+        /// </summary>
+        private void AutoCeratedb()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    connection.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    if (connection.State != System.Data.ConnectionState.Closed)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 查询数据
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns></returns>
+        public DataTable ExecuteQuery(string query)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                {
+                    using (SQLiteDataAdapter adapter = new SQLiteDataAdapter(command))
+                    {
+                        DataTable dataTable = new DataTable();
+                        adapter.Fill(dataTable);
+                        return dataTable;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// 执行语句
+        /// </summary>
+        /// <param name="sqlString">sqlString</param>
+        /// <returns></returns>
+        public bool ExecuteNonQuery(string sqlString)
+        {
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+                {
+                    connection.Open();
+
+                    using (SQLiteCommand command = new SQLiteCommand(sqlString, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //FileLog.LogError("DBError", ex.Message);
+                return false;
+            }
+        }
+    }
+}
